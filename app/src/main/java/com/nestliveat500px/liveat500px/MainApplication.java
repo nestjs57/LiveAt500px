3fec3c31379cd621e72f62382fc4421e6f545354
nestjs57 <seecc0147@gmial.com>
2018-07-10 20:58:59 +0700

    Initial commit

---
+++ app/src/main/java/com/nestliveat500px/liveat500px/MainApplication.java
@@ -0,0 +1,18 @@
+package com.nestliveat500px.liveat500px;
+
+import android.app.Application;
+
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Contextor.getInstance().init(getApplicationContext());
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
---
+++ app/src/main/java/com/nestliveat500px/liveat500px/activity/MainActivity.java
@@ -0,0 +1,75 @@
+package com.nestliveat500px.liveat500px.activity;
+
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.nestliveat500px.liveat500px.R;
+import com.nestliveat500px.liveat500px.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    private android.support.v7.widget.Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initInstances();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contentContainer, MainFragment.newInstance())
+                    .commit();
+        }
+    }
+
+    private void initInstances() {
+
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+
+    }
+}
