5377bcd7b855e6df6b7cf95a270c8f7f68abac0b
nestjs57 <seecc0147@gmial.com>
2018-07-10 23:19:36 +0700

    Set Screen Size of PhotoListItem is 3:2

--- app/src/main/java/com/nestliveat500px/liveat500px/view/PhotoListItem.java
+++ app/src/main/java/com/nestliveat500px/liveat500px/view/PhotoListItem.java
@@ -87,4 +87,16 @@ public class PhotoListItem extends BaseCustomViewGroup {
         // Restore State from bundle here
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = width*2/3;
+        int newHeightSpec = MeasureSpec.makeMeasureSpec(
+                height,
+                MeasureSpec.EXACTLY
+        );
+        super.onMeasure(widthMeasureSpec, newHeightSpec);
+        setMeasuredDimension(width,height);
+    }
 }
